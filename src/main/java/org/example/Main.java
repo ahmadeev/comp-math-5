f2d40604ba3426da75c5a6830d1a34db86be7617
danis <ahrnadeev@gmail.com>
2024-05-24 02:25:50 +0300

    meow

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
 import static org.example.Test.getLagrangePolynomialSum;
+import static org.example.Test.getNewtonPolynomial;
 
 public class Main {
     public static void main(String[] args) {
 
+        //  Лагранж
+
 //        double[] x = {0.1, 0.2, 0.3, 0.4, 0.5};   //  x = 0.35
 //        double[] y = {1.25, 2.38, 3.79, 5.44, 7.14};
 
-        double[] x = {100, 121, 144};   //  x = 105
-        double[] y = {10, 11, 12};
+//        double[] x = {100, 121, 144};   //  x = 105
+//        double[] y = {10, 11, 12};
 
-        System.out.println(getLagrangePolynomialSum(105, x, y));
+        //  Ньютон (неравноотстоящие узлы)
+
+        double[] x = {0.15, 0.2, 0.33, 0.47};   //  x = 0.22
+        double[] y = {1.25, 2.38, 3.79, 5.44};
+
+        //System.out.println(getLagrangePolynomialSum(105, x, y));
+
+        System.out.println(getNewtonPolynomial(0.22, x, y));
 
     }
 }
--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Test {
     public static double getLagrangePolynomialSum(double variableX, double[] x, double[] y) {
         double result = 0;
@@ -22,6 +24,59 @@ public class Test {
             if (i != pointNumber) {
                 result *= (variableX - x[i]) / (x[pointNumber] - x[i]);
             }
+        }
+        return result;
+    }
+
+    public static double getNewtonPolynomial(double variableX, double[] x, double[] y) {
+        int size = x.length;
+
+        //  массив неразделенных сумм (по количеству узлов)
+        double[][] array = new double[size][];
+
+        //  создание и заполнение первого уровня значениями из таблицы
+        array[0] = new double[size];
+        for(int i = 0; i < size; i++) {
+            array[0][i] = y[i];
+        }
+
+        //  создание остальных уровней
+        for(int i = 1; i < size; i++) {
+            array[i] = new double[size - i];
+        }
+
+        //  заполнение остальных уровней
+        for(int i = 1; i < size; i++) {
+            for(int j = 0; j < array[i].length; j++) {
+                array[i][j] = (array[i-1][j+1] - array[i-1][j]) / (x[i+j] - x[j]);
+            }
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < size; i++) {
+            System.out.println(Arrays.toString(array[i]));
+        }
+
+        System.out.println();
+
+        //  вычисление значения полинома в точке
+        double result = 0;
+        for(int i = 0; i < size; i++) {
+            result += array[i][0] * getMultipleForNewtonPolynomial(i, variableX, x);
+        }
+        return result;
+    }
+
+    /** Вычисление в многочлене Ньютона множителей, содержащих переменную x.
+     * position -- позиция слагаемого в многочлене,
+     * variableX -- значение переменной x,
+     * x -- массив значений координаты x из таблицы.
+     */
+    public static double getMultipleForNewtonPolynomial(int position, double variableX, double[] x) {
+        double result = 1;
+        for(int i = 0; i < position; i++) {
+            result *= variableX - x[position - 1];
         }
         return result;
     }
