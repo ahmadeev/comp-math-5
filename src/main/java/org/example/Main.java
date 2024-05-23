cd8c71bce764069af70b4989f915b3c63ea8ff05
danis <ahrnadeev@gmail.com>
2024-05-24 01:42:38 +0300

    meow

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
-import static org.example.Test.getResult;
+import static org.example.Test.getLagrangePolynomialSum;
 
 public class Main {
     public static void main(String[] args) {
 
-//        double[] x = {0.1, 0.2, 0.3, 0.4, 0.5};
+//        double[] x = {0.1, 0.2, 0.3, 0.4, 0.5};   //  x = 0.35
 //        double[] y = {1.25, 2.38, 3.79, 5.44, 7.14};
 
-        double[] x = {100, 121, 144};
+        double[] x = {100, 121, 144};   //  x = 105
         double[] y = {10, 11, 12};
 
-        getResult(105, x, y);
+        System.out.println(getLagrangePolynomialSum(105, x, y));
 
     }
 }
--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,27 +1,25 @@
 package org.example;
 
 public class Test {
-    public static void getResult(double variableX, double[] x, double[] y) {
-
+    public static double getLagrangePolynomialSum(double variableX, double[] x, double[] y) {
         double result = 0;
-
         for(int i = 0; i < x.length; i++) {
-
-            result += y[i] * getLValue(x, variableX, i);
-
+            result += y[i] * getLagrangePolynomial(x, variableX, i);
         }
-        System.out.println(result);
-
+        return result;
     }
 
-    public static double getLValue(double[] x, double variableX, int pointNumber) {
+    /**  Метод для вычисления многочлена Лагранжа.
+     * x -- массив значений координаты x из таблицы,
+     * variableX -- точка, в которой нужно вычислить значение многочлена Лагранжа,
+     * pointNumber -- позиция слагаемого в многочлене Лагранжа
+     */
+    public static double getLagrangePolynomial(double[] x, double variableX, int pointNumber) {
         int size = x.length;
 
         double result = 1;
         for(int i = 0; i < size; i++) {
-            if (i == pointNumber) {
-                continue;
-            } else {
+            if (i != pointNumber) {
                 result *= (variableX - x[i]) / (x[pointNumber] - x[i]);
             }
         }
